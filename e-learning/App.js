cf1fc50cccff0d2fd5a154fb3c7e6dda37e5fdcd
AswinSTS <aswin.s@deltasoft.in>
2023-12-20 13:36:17 +0530

    basic expo app installed

---
+++ e-learning/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  });
+  return (
+    <View style={styles.container}>
+      <Text>hello aswins how are you?</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
